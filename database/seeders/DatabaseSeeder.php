e06a0e10e0f3be66f11a88db4a78c38739064f87
LuisitRoque3 <37349435+LuisitRoque3@users.noreply.github.com>
2026-06-29 23:30:37 -0600

    Fix seeder for production without dev dependencies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'name' => 'Control de Tráfico',
             'email' => 'tc@trax.com',
             'password' => bcrypt('1234'),
             'role' => 'traffic_control'
         ]);
 
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'name' => 'Operador 1',
             'email' => 'op1@trax.com',
             'password' => bcrypt('1234'),
